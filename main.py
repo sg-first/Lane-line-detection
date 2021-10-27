cd9aa67f0d45bb59da795fd73ccba6131c039940
3A <524019351@qq.com>
2021-10-27 11:45:15 +0800

    都试了一遍

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('0.png',0)
+def show(img):
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+
+img = cv2.imread('9.png',0)
 h, w = img.shape
-img = cv2.GaussianBlur(img,(3, 3),0)
-img = cv2.Canny(img,160,180) # min,max
-lines = cv2.HoughLines(img, 1, np.pi / 180, 300) # thr
+img = cv2.GaussianBlur(img,(5, 5),0)
+# 5杂线很多，得调滤波器到7（但会滤掉车道线（主要是短线的问题））
+# 8短线检测不出来（整体算法的问题，得加东西）
+img = cv2.Canny(img,10,200) # min,max
+# 3:200, 300
+# 4:40, 50（阴影非全，而且现在的结果也有点问题）
+# show(img)
+# 进行花样滤波？
+lines = cv2.HoughLines(img, 1, np.pi / 180, 290) # thr
 
 img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 for i in range(len(lines)):
@@ -21,5 +31,4 @@ for i in range(len(lines)):
 
         cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-cv2.imshow('img',img)
-cv2.waitKey(0)
+show(img)
